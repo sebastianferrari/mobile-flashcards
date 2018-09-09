f9a676baa1f81b2d1b32b1a47211ad700927faac
Sebastian Ferrari <sebastianferrari@live.com>
2018-09-08 19:51:52 -0500

    feat: New question validation

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,33 +1,67 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native'
-import { gray, black, white } from '../utils/colors'
+import { gray, black, white, red } from '../utils/colors'
 
 class NewQuestion extends Component {
   state = {
     question: '',
-    answer: ''
+    answer: '',
+    invalidQuestion: false,
+    invalidAnswer: false
+  }
+
+  addQuestion = () => {
+    // console.log('Adding question...')
+    const { question, answer } = this.state
+
+    // Validate first
+    if (question === '' || answer === '') {
+      if (question === '') {
+        this.setState({ invalidQuestion: true })
+      } else {
+        this.setState({ invalidQuestion: false })
+      }
+      if (answer === '') {
+        this.setState({ invalidAnswer: true })
+      } else {
+        this.setState({ invalidAnswer: false })
+      }
+      return
+    }
+
+    this.setState({
+      invalidAnswer: false,
+      invalidQuestion: false
+    })
+
+    
+
+
+
+    // route
   }
 
   render() {
     return (
       <View style={styles.container}>
         <TextInput
-          style={ styles.textInput }
+          style={ [styles.textInput, this.state.invalidQuestion ? styles.invalidTextInput : '' ] }
           onChangeText={(question) => this.setState({ question })}
           value={this.state.question}
           placeholder='Question'
         />
         <TextInput
-          style={styles.textInput}
+          style={ [styles.textInput, this.state.invalidAnswer ? styles.invalidTextInput : '' ]}
           onChangeText={(answer) => this.setState({ answer })}
           value={this.state.answer}
           placeholder='Answer'
         />
 
         <TouchableOpacity
-          onPress={() => this.props.navigation.navigate(
-            'DeckDetail'
-          )}
+          // onPress={() => this.props.navigation.navigate(
+          //   'DeckDetail'
+          // )}
+          onPress={this.addQuestion}
           style={styles.submitBtn}
         >
           <Text style={{ color: white, fontSize: 18 }}>Submit</Text>
@@ -62,6 +96,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 20
+  },
+  invalidTextInput: {
+    borderColor: red,
+    borderWidth: 3
   }
 })
 
