d34d3b4900e95757af24f5bfbbc957ceae4e0927
Sebastian Ferrari <sebastianferrari@live.com>
2018-09-09 13:25:09 -0500

    feat: Add some logs from api

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,8 @@
 import { AsyncStorage } from 'react-native'
 import { DECKS_STORAGE_KEY, loadDecks } from './_decks'
 
+const prefix = '[[api.js]] === '
+
 export function fetchDummyDecks() {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY)
     .then(loadDecks)
@@ -11,19 +13,19 @@ export function getDecks() {
 }
 
 export function getDeck(id) {
-  console.log('Getting deck id: ' + id)
+  console.log(`${prefix}Getting deck id: ${id}`)
   return decks[id]
 }
 
 export function saveDeckTitle({ key, deck }) {
-  console.log(`Saving new deck with key/title: ${key}`)
+  console.log(`${prefix}Saving new deck with key/title: ${key}`)
   return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
     [key]: deck
   }))
 }
 
 export function addCardToDeck(title, card) {
-  console.log(`Adding card ${JSON.stringify(card)} to deck ${title}`)
+  console.log(`${prefix}Adding card ${JSON.stringify(card)} to deck ${title}`)
   return AsyncStorage.getItem(DECKS_STORAGE_KEY)
     .then((results) => {
       const data = JSON.parse(results)
